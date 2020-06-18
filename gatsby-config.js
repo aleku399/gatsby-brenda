33cc135ccbed852f38499c4ff283b4fccaf23758
aleku399 <aleku399@gmail.com>
2020-06-18 08:11:41 +0300

    feat: change description

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,7 @@ module.exports = {
   siteMetadata: {
     title: 'Nabalisa Foundation',
     description:
-      'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
+      'Lorem ipsum odor amet, consectetuer adipiscing elit. Ac purus in massa egestas mollis varius.',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
